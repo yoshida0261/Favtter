a7eb675224581b6bfa7dac8e276ead4d92e1528e
yoshida0261 <yoshida261@gmail.com>
2016-09-14 03:01:05 +0900

    java to kotlin

---
+++ MainActivity.kt
@@ -0,0 +1,12 @@
+package com.starcompany.favtter
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
